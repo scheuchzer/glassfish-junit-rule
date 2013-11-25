b276cc8f893d0d4687c63536633a3a1e49400364
scheuchzer <thomas.scheuchzer@gmx.net>
2013-11-25 21:45:18 +0100

    some commands

--- src/main/java/com/ja/junit/rule/glassfish/StartupCommand.java
+++
@@ -1,6 +0,0 @@
-package com.ja.junit.rule.glassfish;
-
-public interface StartupCommand {
-
-	void execute(final TestContext ctx) throws Exception;
-}
--- src/main/java/com/ja/junit/rule/glassfish/TeardownCommand.java
+++
@@ -1,6 +0,0 @@
-package com.ja.junit.rule.glassfish;
-
-public interface TeardownCommand {
-
-	void execute(final TestContext ctx) throws Exception;
-}
